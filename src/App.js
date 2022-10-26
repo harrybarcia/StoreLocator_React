a3f1ea527beaf649fb2bda665a811160a6b01916
harrybarcia12 <barcia.harry@gmail.com>
2022-10-25 23:09:54 -0700

    adding the map and stores on the FE side

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-import React, {useState, useEffect, useRef} from 'react';
-import { Helmet} from 'react-helmet';
-import mapboxgl from 'mapbox-gl';
-import DisplayMap from './components/map';
+import React from 'react';
+import MapData from './components/map';
+import Child from './components/parent';
 
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiaGFycnliYXJjaWEiLCJhIjoiY2s3dzRvdTJnMDBqODNlbzhpcjdmaGxldiJ9.vg2wE4S7o_nryVx8IFIOuQ';
 
 
 
 const App = () => {
-	const mapContainer = useRef(null);
-	const map = useRef(null);
-	const [lng, setLng] = useState(-70.9);
-	const [lat, setLat] = useState(42.35);
-	const [zoom, setZoom] = useState(9);
 
-	useEffect(() => {
-		if (map.current) return; // initialize map only once
-		map.current = new mapboxgl.Map({
-			container: mapContainer.current,
-			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [lng, lat],
-			zoom: zoom
-		});
-	});
 		return (
 			<div>
-				<div ref={mapContainer} className="map-container" />
-				<DisplayMap></DisplayMap>
+				{/* me retourne la carte */}
+				<Child></Child>
+				{/* me retourne les stores */}
+				<MapData></MapData>
 			</div>
 		);
 		
--- src/components/map.js
+++ src/components/map.js
@@ -1,12 +1,8 @@
-import React, {useState, useEffect} from 'react';
-
-
-const DisplayMap = () => {
-    const mystyle = {
-        height: "200px",
-        width: "200px",
-        borderStyle: "solid"};
-
+import React, {useState, useEffect, useRef} from 'react';
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFycnliYXJjaWEiLCJhIjoiY2s3dzRvdTJnMDBqODNlbzhpcjdmaGxldiJ9.vg2wE4S7o_nryVx8IFIOuQ';
+const DisplayMap = (props) => {
+    
 const [backendData, setBackendData] = useState(null);
 useEffect(() => {
     // declare the async data fetching function
@@ -15,19 +11,102 @@ useEffect(() => {
       const data = await fetch('/api');
       // convert the data to json
       const json = await data.json();
+      console.log(json);
+      console.log('--');
   
       // set state with the result
       setBackendData(json);
     }
-  console.log(backendData);
+  
     // call the function
     fetchData()
       // make sure to catch any error
       .catch(console.error);;
   }, [])
 
-  return (	
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-123.1207);
+  const [lat, setLat] = useState(49.28);
+  const [zoom, setZoom] = useState(10);
+  
+  useEffect(() => {
+  
+        const map = new mapboxgl.Map({
+      
+          container: mapContainer.current,
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [lng, lat],
+          zoom: zoom
+      });
+      function getStores() {
+        try{
+          console.log("data", backendData);
+          const stores = backendData.map(store => {
+            return {
     
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [
+                  store.location.coordinates[0],
+                  store.location.coordinates[1]
+                ]
+              },
+              properties: {
+                _id: store._id,
+                storeId: store.storeId,
+                formattedAddress:store.location.formattedAddress,  
+                icon: 'rocket',
+                image:store.image,
+                userId:store.userId,
+                city:store.city
+              }
+            };
+          });
+          console.log("stores", stores);
+          loadMap(stores);
+        }
+        catch(err){
+          console.log(err);
+        }
+    }
+    // Load map with stores
+function loadMap(stores) {
+    map.on('load', function() {
+      map.addLayer({
+        id: 'points',
+        type: 'symbol',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: stores
+          }
+        },
+        layout: {
+          'icon-image': '{icon}-15',
+          'icon-size': 1.5,
+          'text-field': '{city}',
+          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+          'text-offset': [0, 0.9],
+          'text-anchor': 'top'
+        }
+      });
+    });
+    
+  
+  }
+  getStores();
+
+    
+      
+  }, 
+  [backendData]);
+
+  return (	
+    <div>
+        <div ref={mapContainer} className="map-container"></div>
     <div>   
         {( backendData && backendData.length > 0) ? backendData.map((store, index) => {
         
@@ -39,14 +118,14 @@ useEffect(() => {
                     </div>
                     
                 </div>
-
             );
-
         })
         : <p>loading...</p>
         };
-        
     </div>
+
+    </div>
+    
     );
 }
 export default DisplayMap;
