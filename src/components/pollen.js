5c83a169ce423b149bf30300de1301ceff1a7f91
harrybarcia12 <barcia.harry@gmail.com>
2023-01-05 17:01:36 -0800

    starting with pollen layer

--- src/components/pollen.js
+++ src/components/pollen.js
@@ -4,15 +4,22 @@ import './pollen.css';
 import { Link } from "react-router-dom";
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import axios from "axios";
 mapboxgl.accessToken = "pk.eyJ1IjoiaGFycnliYXJjaWEiLCJhIjoiY2s3dzRvdTJnMDBqODNlbzhpcjdmaGxldiJ9.vg2wE4S7o_nryVx8IFIOuQ";
 
 const Pollen = () => {
 
     const mapContainer = useRef(null);
     const geocoderContainer = useRef(null);
-    
-    useEffect(() => {
+    const [backendData, setBackendData] = useState(null);
 
+
+
+
+
+
+
+    useEffect(() => {
         const map = new mapboxgl.Map({
             container: mapContainer.current, // container id
             style: 'mapbox://styles/mapbox/streets-v11',
@@ -85,76 +92,76 @@ const Pollen = () => {
                     type: 'vector',
                     url: 'mapbox://harrybarcia.3hys6vpn' //tilesetID
                 });
-                map.addLayer({
-                    'id': 'cities',
-                    'type': 'fill',
-                    'source': 'cities',
-                    'source-layer': 'cities-a5mbav',
-                    'layout':{
-                        'visibility':'visible'
-                    } ,
-                    'paint': {
-                        'fill-color': "#3bb2d0",
-                        'fill-opacity': 0.5,
-                        'fill-outline-color': "#000000"
-                    }
-                });
+
 
 
             });
 
 
 
-            // After the last frame rendered before the map enters an "idle" state.
-    map.on("idle", () => {
-      // If these two layers were not added to the map, abort
-      if (!map.getLayer("provinces") || !map.getLayer("cities")) {
-        return;
-      }
+                        // After the last frame rendered before the map enters an "idle" state.
+                map.on("idle", () => {
+                  // If these two layers were not added to the map, abort
+                  if (!map.getLayer("provinces") || !map.getLayer("cities")) {
+                    return;
+                  }
 
-      // Enumerate ids of the layers.
-      const toggleableLayerIds = ["provinces", "cities"];
-      console.log("toggleableLayerIds", toggleableLayerIds);
-      // Set up the corresponding toggle button for each layer.
-      for (const id of toggleableLayerIds) {
-        // Skip layers that already have a button set up.
-        if (document.getElementById(id)) {
-          continue;
-        }
+                  // Enumerate ids of the layers.
+                  const toggleableLayerIds = ["provinces", "cities"];
+                  console.log("toggleableLayerIds", toggleableLayerIds);
+                  // Set up the corresponding toggle button for each layer.
+                  for (const id of toggleableLayerIds) {
+                    // Skip layers that already have a button set up.
+                    if (document.getElementById(id)) {
+                      continue;
+                    }
 
-        // Create a link.
-        const link = document.createElement("a");
-        link.id = id;
-        link.href = "#";
-        link.textContent = id;
-        link.className = "active";
+                    // Create a link.
+                    const link = document.createElement("a");
+                    link.id = id;
+                    link.href = "#";
+                    link.textContent = id;
+                    link.className = "active";
 
-        // Show or hide layer when the toggle is clicked.
-        link.onclick = function (e) {
-          const clickedLayer = this.textContent;
-          console.log("clickedLayer", clickedLayer);
-          e.preventDefault();
-          e.stopPropagation();
+                    // Show or hide layer when the toggle is clicked.
+                    link.onclick = function (e) {
+                      const clickedLayer = this.textContent;
+                      console.log("clickedLayer", clickedLayer);
+                      e.preventDefault();
+                      e.stopPropagation();
 
-          const visibility = map.getLayoutProperty(clickedLayer, "visibility");
+                      const visibility = map.getLayoutProperty(clickedLayer, "visibility");
 
-          // Toggle layer visibility by changing the layout object's visibility property.
-          if (visibility === "visible") {
-            map.setLayoutProperty(clickedLayer, "visibility", "none");
-            this.className = "";
-          } else {
-            this.className = "active";
-            map.setLayoutProperty(clickedLayer, "visibility", "visible");
-          }
-        };
+                      // Toggle layer visibility by changing the layout object's visibility property.
+                      if (visibility === "visible") {
+                        map.setLayoutProperty(clickedLayer, "visibility", "none");
+                        this.className = "";
+                      } else {
+                        this.className = "active";
+                        map.setLayoutProperty(clickedLayer, "visibility", "visible");
+                      }
+                    };
 
-        const layers = document.getElementById("menu");
-        console.log("layers", layers);
-        layers.appendChild(link);
-      }
-    });
+                    const layers = document.getElementById("menu");
+                    console.log("layers", layers);
+                    layers.appendChild(link);
+                  }
+                });
+
+                const fetchData = async () => {
+                  const response = axios(
+                    'http://localhost:3000/pollens',)
+                    .then(response => {
+                      
+                      setBackendData(response.data.data);
+                    })      
+                };
+                fetchData();
+              console.log("backendData", backendData);
  
-    }, []);
+    }, [
+        
+    ]);
 
     
 
@@ -166,7 +173,19 @@ const Pollen = () => {
             <div ref={mapContainer}></div>
           </div>
           <div className="menu" id="menu"></div>
-         </div>
+          <div className="footer">
+            {(backendData && backendData.length>0) ? backendData.map((item) => {
+              return (
+                <div>
+                 
+                    {item.province}
+
+                  </div>
+
+              )}
+            ) : "Loading..."}
+          </div>
+        </div>
     );
 };
 
