9c152fe03bb1813ca5af2fb44638084d7cbfd5cd
harrybarcia12 <barcia.harry@gmail.com>
2023-01-07 22:38:37 -0800

    adding the geojson layers from the database

--- routes/pollen.js
+++ routes/pollen.js
@@ -8,4 +8,9 @@ router.get("/pollens", async function(req, res, next) {
     res.status(200).json({ data });
 });
 
+router.get("/places", async function(req, res, next) {
+    const data = await db.collection('places').find().toArray();
+    res.status(200).json({ data });
+});
+
 module.exports = router;
