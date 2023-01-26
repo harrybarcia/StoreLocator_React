749b70adc7c7a440fd27b488daf1744f50e4315d
harrybarcia12 <barcia.harry@gmail.com>
2023-01-25 18:00:10 -0800

    new token validation

--- utils/JWT.js
+++ utils/JWT.js
@@ -2,14 +2,20 @@
 const jwt = require('jsonwebtoken')
 function authenticateToken(req, res, next) {
   console.log('req.headers', req.headers);
-  console.log('req.headers split', req.headers['cookie'].split('=')[1]);
-
+  console.log('---');
+  console.log('req.headers split', req.headers['cookie'].split('=')[2]);
+  
+  console.log('---');
+  console.log('---');
   const authHeader = req.headers['cookie']
-  const token = authHeader && authHeader.split('=')[1]
+  const token = authHeader && authHeader.split('=')[2]
   if (token == null) return res.sendStatus(401)
-
+  
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(token)
+    console.log('---');
+    
+    console.log("token", token)
+    console.log('---');
     console.log(process.env.ACCESS_TOKEN_SECRET)
     console.log(err)
     if (err) return res.sendStatus(403)
