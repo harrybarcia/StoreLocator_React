2867467bbd780e77e5255b62fba77b55d5ae315a
harrybarcia12 <barcia.harry@gmail.com>
2023-01-27 14:29:03 -0800

    "pushing on both branches[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[

--- push.sh
+++ push.sh
@@ -1,6 +1,5 @@
 git add .
 read -p "Enter commit message: " commit_message
 git commit -m "$commit_message"
-BRANCH=$(git describe --contains --all HEAD)
-echo 'the name of the commit is : ' $commit_message ', it will be pushed on the branch ' BRANCH
-git push origin "$BRANCH"
+git push origin api
+git push origin app
