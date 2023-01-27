dc09fbeabd404c52e0c5fab3d856b2c6b27f6123
harrybarcia12 <barcia.harry@gmail.com>
2023-01-27 14:21:27 -0800

    test

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git add .
 read -p "Enter commit message: " commit_message
 git commit -m "$commit_message"
-echo 'the name of the commit is : $commit_message"'
+echo 'the name of the commit is : ' $commit_message
 git push origin "$BRANCH"
