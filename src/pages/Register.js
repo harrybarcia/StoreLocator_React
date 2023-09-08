45a0b8dbb679b64b8228b96ac6edf1e1cfd924c7
harrybarcia12 <barcia.harry@gmail.com>
2023-09-07 19:31:36 -0700

    debut of authentication admin

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,30 +1,29 @@
 import axios from 'axios';
-import {useRef, useState, useEffect} from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-
-const SimpleInput2 = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const SignUpComponent = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState('');
-
-  
   const navigate = useNavigate();
 
-  const handleEmailChange = (evt) => {
-    setEmail(evt.target.value);
-    
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
-  const handlePasswordChange = (evt) => {
-    setPassword(evt.target.value);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
   };
+
   const handleConfirmPasswordChange = (evt) => {
     setConfirmPassword(evt.target.value);
   };
-  
-  const handleSubmit2 = async (evt) => {
-    evt.preventDefault();
-    
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
     axios({
       method: 'post',
       url: '/register',
@@ -36,56 +35,64 @@ const SimpleInput2 = () => {
     })
     .then(function (response) {
       console.log(response);
-    } )
-    
-    
-  }
+      navigate("/");
 
+    } )
+  };
 
   return (
-    <div>
+    <div className="flex items-center justify-center h-screen">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Your Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
 
-    <form  
-    style={{display: 'flex', flexDirection: 'column', width: '300px', margin: "40px"}}
-    onSubmit={handleSubmit2}>
-      <div >
-        <label >Your Email</label>
-        <input 
-        type="text" 
-        name="email"
-        value={email} 
-        onChange={handleEmailChange} />
-        
-       
-        
-      </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
 
-      <div >
-        <label >Password</label>
-        <input
-        type="password"
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
+            Confirm Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+          />
+        </div>
 
-        name="password"
-        value={password}
-        onChange={handlePasswordChange}
-        />
-      </div>
-
-      <div>
-        <label> Confirm Password</label>
-        <input
-        type="password"
-        name="confirmPassword"
-        value={confirmPassword}
-        onChange={handleConfirmPasswordChange}
-        />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
+        <div className="flex items-center justify-between">
+          <button
+            className="custom-button"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
 
-export default SimpleInput2;
+export default SignUpComponent;
