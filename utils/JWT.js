6feb0efee7a6918c27f62e999625a937c7a69bc1
unknown <barcia.harry@gmail.com>
2023-10-20 15:36:37 -0700

    login problem on jwt solved

--- utils/JWT.js
+++ utils/JWT.js
@@ -2,8 +2,10 @@ const jwt = require('jsonwebtoken')
 function authenticateToken(req, res, next) {
   console.log('req.headers', req.headers);
   const authHeader = req.headers['cookie']
-  console.log("1", authHeader)
-  const token = authHeader.split('=')[1]
+  console.log(authHeader.split('=')[1])
+  console.log(req.headers)
+  console.log("1", authHeader.split(';')[1].split('=')[1])
+  const token = authHeader.split(';')[1].split('=')[1]
   if (token == null) return res.sendStatus(401)
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403)
